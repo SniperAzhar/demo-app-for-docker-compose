099cd642dfdd326779da39021fe5576324aab369
Stewie2k46 <stewie2k956@gmail.com>
2024-08-03 22:52:09 +0530

    Update app.py

--- application/frontend/myweb/app.py
+++ application/frontend/myweb/app.py
@@ -9,9 +9,7 @@ from redis import Redis
 
 app = Flask(__name__)
 
-# Get Redis connection details from environment variables
-redis_host = os.getenv('REDIS_HOST', 'localhost')  # Default to 'localhost' if not set
-redis_port = int(os.getenv('REDIS_PORT', 6379))    # Default to 6379 if not set
+redisurl = os.getenv('REDIS_URL')
 hostname = socket.gethostname()
 
 # Button Colour
@@ -21,7 +19,7 @@ button = './static/{}.png'.format(buttoncolour)
 
 def get_redis():
     if not hasattr(g, 'redis'):
-        g.redis = Redis(host=redis_host, port=redis_port, db=0, socket_timeout=5,
+        g.redis = Redis(host=redisurl, db=0, socket_timeout=5,
                         decode_responses=True)
     return g.redis
 
